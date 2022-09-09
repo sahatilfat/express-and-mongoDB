b570c30a0934474378c9206b7ec083ad01c6ef34
sahatreact <sahatreactjs@gmail.com>
2022-09-09 21:41:43 +0700

    Refactoring Our Route

--- app.js
+++ app.js
@@ -5,20 +5,11 @@ const app = express();
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: "Hello from the server side!", app: "Natours" });
-// });
-// app.post("/", (req, res) => {
-//   res.send("you can post to this endpoint...");
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     result: tours.length,
@@ -26,9 +17,29 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const getTour = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1; //ini berguna utk convert nilai params menjadi number
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -47,29 +58,9 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
-});
+};
 
-app.get("/api/v1/tours/:id", (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1; //ini berguna utk convert nilai params menjadi number
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
-});
-
-app.patch("/api/v1/tours/:id", (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "fail",
@@ -83,9 +74,9 @@ app.patch("/api/v1/tours/:id", (req, res) => {
       tour: "<Updated tour here...>",
     },
   });
-});
+};
 
-app.delete("/api/v1/tours/:id", (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "fail",
@@ -97,7 +88,14 @@ app.delete("/api/v1/tours/:id", (req, res) => {
     status: "success",
     data: null,
   });
-});
+};
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
