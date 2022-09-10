eb7a8aaf912ba97cba9e7b8faf4affa0acfebc03
sahatreact <sahatreactjs@gmail.com>
2022-09-10 14:03:09 +0700

    Implementing the Users Routes:

--- app.js
+++ app.js
@@ -108,6 +108,41 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This  route is not yet defined!",
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This  route is not yet defined!",
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This  route is not yet defined!",
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This  route is not yet defined!",
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This  route is not yet defined!",
+  });
+};
+
 // 3) ROUTES
 
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
@@ -116,6 +151,13 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route("/api/v1/users").get(getAllUsers).post(createUser);
+app
+  .route("/api/v1/users/:id")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // 4) STARTS SERVER
 
