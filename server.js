a50fab1cf80dc933a40f1f6e9ca79b7ec7d14963
sahatreact <sahatreactjs@gmail.com>
2022-10-08 11:54:08 +0700

    Connecting our database with the Express app

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-const app = require("./app");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app'); // ini untuk require express application
 
+dotenv.config({ path: './config.env' }); // ini untuk require environment
 // console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
