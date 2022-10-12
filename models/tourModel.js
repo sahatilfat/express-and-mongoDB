08f18fc525254fbd329dfe8cd3958660e448f2ee
sahatreact <sahatreactjs@gmail.com>
2022-10-12 13:41:41 +0700

    Modelling the Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a durations'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour mush have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour mush have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // ini berguna utk menghapus whitespace
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // ini merupakan contoh model
