919b6566e0d5df32b6c339e22d6cf836a8c3d845
sahatreact <sahatreactjs@gmail.com>
2022-10-27 09:46:56 +0700

    Making The API Better Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,14 +3,20 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     //* BUILD QUERY
+    //* 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    //* 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(queryObj);
+    const query = Tour.find(JSON.parse(queryStr));
 
+    //* EXECUTE QUERY
+    const tours = await query;
     // const query = await Tour.find({
     //   duration: 5,
     //   difficulty: 'easy'
@@ -21,9 +27,6 @@ exports.getAllTours = async (req, res) => {
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    //* EXECUTE QUERY
-    const tours = await query;
 
     // * SEND RESPONSE
     res.status(200).json({
